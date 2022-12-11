42fd852de40f3f059671b28e64ed39bba503044b
Jarno Varis <jarno.varis@gmail.com>
2022-12-11 11:34:36 +0200

    added input validation, and it seems to work, and POST:ing through test-req-file and UI still works

--- backend/src/routes/patients.ts
+++ backend/src/routes/patients.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import express from 'express';
 import patientService from '../services/patientService';
+import toNewPatientEntry from '../utils';
 
 const router = express.Router();
 
@@ -9,17 +12,19 @@ router.get('/', (_req, res) => {
 
 router.post('/', (req, res) => {
 
-  const { name, dateOfBirth, ssn, gender, occupation } = req.body
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const newPatientEntry = toNewPatientEntry(req.body);
 
-  const newPatientEntry = patientService.addPatient({
-    name,
-    dateOfBirth,
-    ssn,
-    gender,
-    occupation
-  });
-
-  res.json(newPatientEntry);
+    const addedEntry = patientService.addPatient(newPatientEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
--- backend/src/services/patientService.ts
+++ backend/src/services/patientService.ts
@@ -1,22 +1,24 @@
 import patientData from '../../data/patients.json';
 
-import { NonSensitivePatientEntry, PatientEntry, NewPatientEntry } from '../types';
+import { NewPatientEntry, NonSensitivePatientEntry, PatientEntry } from '../types';
 
-import { v1 as uuid } from 'uuid'
+import { v1 as uuid } from 'uuid';
 
 const patients: Array<PatientEntry> = patientData;
 
 const getNonSensitiveEntries = (): Array<NonSensitivePatientEntry> => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   return patients.map(({ssn, ...keepProperties}) => keepProperties);
 };
 
 const addPatient = (
   entry: NewPatientEntry
 ): PatientEntry => {
+  
   const newPatientEntry = {
     id: uuid(),
     ...entry
-  }
+  };
 
   patients.push(newPatientEntry);
   return newPatientEntry;
